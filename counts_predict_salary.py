3574cf3aedcb08d42130af52d9bd5fa16b9ee41e
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-08-31 20:33:47 +0300

    reduced the code

--- counts_predict_salary.py
+++ counts_predict_salary.py
@@ -1,9 +1,7 @@
 def predict_salary(salary_from, salary_to):
-    salary = 0
     if salary_from and salary_to:
-        salary += (salary_from + salary_to) / 2
+        return (salary_from + salary_to) / 2
     elif salary_from:
-        salary += salary_from * 1.2
+        return salary_from * 1.2
     elif salary_to:
-        salary += salary_to * 0.8
-    return salary
+        return salary_to * 0.8
