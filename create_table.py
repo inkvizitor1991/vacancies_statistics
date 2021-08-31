21cce7a5e8d0b340659f2dad824564bfd6eb4a54
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-08-31 20:24:05 +0300

    deleted print() and moved to ifmain

--- create_table.py
+++ create_table.py
@@ -1,7 +1,7 @@
 from terminaltables import AsciiTable
 
 
-def create_table(parsed_vacancies, name):
+def get_statistics(parsed_vacancies, name):
     statistics = [('Язык программирования ', 'Вакансий найдено',
                    'Вакансий обработано', 'Средняя зарплата')]
     for language in parsed_vacancies:
@@ -9,8 +9,12 @@ def create_table(parsed_vacancies, name):
             (language, parsed_vacancies[language]['vacancies_found'],
              parsed_vacancies[language]['vacancies_processed'],
              parsed_vacancies[language]['average_salary']))
-    title = f'{name} Moscow'
-    table_instance = AsciiTable(tuple(statistics), title)
-    table_instance.justify_columns[2] = 'right'
-    return table_instance.table
+    create_table(tuple(statistics), name)
 
+
+def create_table(statistics, name):
+    title = f'{name} Moscow'
+    table_instance = AsciiTable(statistics, title)
+    table_instance.justify_columns[2] = 'right'
+    print(table_instance.table)
+    print()
