8e482f821975ac2f4bc438bcdcd3dfe586d94d6c
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-09-01 13:46:58 +0300

    reduced the number of indexes

--- create_table.py
+++ create_table.py
@@ -1,16 +1,20 @@
 from terminaltables import AsciiTable
 
 
-def create_table(parsed_vacancies, name):
-    statistics = [('Язык программирования ', 'Вакансий найдено',
-                   'Вакансий обработано', 'Средняя зарплата')]
-    for language in parsed_vacancies:
-        statistics.append(
-            (language, parsed_vacancies[language]['vacancies_found'],
-             parsed_vacancies[language]['vacancies_processed'],
-             parsed_vacancies[language]['average_salary']))
+def create_table(statistics, name):
+    table_description = [(
+        'Язык программирования ', 'Вакансий найдено',
+        'Вакансий обработано', 'Средняя зарплата'
+    )]
+    for languages in statistics:
+        for language in languages:
+            vacancies_found, vacancies_processed, average_salary = languages[
+                language].values()
+
+            table_description.append((language, vacancies_found,
+                                      vacancies_processed, average_salary))
+
     title = f'{name} Moscow'
-    table_instance = AsciiTable(tuple(statistics), title)
+    table_instance = AsciiTable(tuple(table_description), title)
     table_instance.justify_columns[2] = 'right'
     return table_instance.table
-
