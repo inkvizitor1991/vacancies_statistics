299aee24f8d772a1973123af4f420df74c906aa9
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-09-03 17:48:27 +0300

    created new file which collects language statistics

---
+++ collects_language_statistics.py
@@ -0,0 +1,9 @@
+def get_statistics(language, vacancies_found,
+                   vacancies_processed, vacancies_salary):
+    language_statistics = {}
+    language_statistics[language] = {
+        'vacancies_found': vacancies_found,
+        'vacancies_processed': vacancies_processed,
+        'average_salary': int(vacancies_salary / vacancies_processed)
+    }
+    return language_statistics
