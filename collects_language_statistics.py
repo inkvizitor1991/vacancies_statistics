320f8e37631ba238a01b96ca513c13d5084b38d2
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2021-09-08 23:12:50 +0300

    fixed the last change  if vacancies_processed equal 0

--- collects_language_statistics.py
+++ collects_language_statistics.py
@@ -1,10 +1,13 @@
 def get_statistics(language, vacancies_found,
                    vacancies_processed, vacancies_salary):
     if vacancies_processed:
-        language_statistics = {}
-        language_statistics[language] = {
-            'vacancies_found': vacancies_found,
-            'vacancies_processed': vacancies_processed,
-            'average_salary': int(vacancies_salary / vacancies_processed)
-        }
-        return language_statistics
+        average_salary = int(vacancies_salary / vacancies_processed)
+    else:
+        average_salary = 'Вакансий нет'
+    language_statistics = {}
+    language_statistics[language] = {
+        'vacancies_found': vacancies_found,
+        'vacancies_processed': vacancies_processed,
+        'average_salary': average_salary
+    }
+    return language_statistics
